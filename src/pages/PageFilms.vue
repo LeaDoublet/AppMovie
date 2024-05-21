47e93366834a4486fcfb3160365112cb195a4bf9
Léa Doublet <leadbtcode@gmail.com>
2024-05-21 15:11:27 +0200

    Initial commit

---
+++ src/pages/PageFilms.vue
@@ -0,0 +1,5 @@
+<template>
+    <v-container>Films</v-container>
+</template>
+<script setup>
+</script>
---
+++ src/pages/PageItem2.vue
@@ -0,0 +1,5 @@
+<template>
+    <v-container>Items (2)</v-container>
+</template>
+<script setup>
+</script>
